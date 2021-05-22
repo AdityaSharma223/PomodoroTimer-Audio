2d8e8609e125bcda2feba4600fcf6bc637be0499
Aditya Sharma <46065796+AdityaSharma223@users.noreply.github.com>
2021-05-22 17:08:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import time
+import pygame
+import math
+import random 
+import os
+import sys
+#---------------------------
+
+#-----VARS--------------------
+swidth, sheight = 300, 200 
+run = True
+s, m = 0, 0 
+timel = int(input("""
+Hi and welcome to PyTimer
+Please type (in minutes) how long u want the time to be
+here: """))
+
+#-----------------------------
+print(hours, minutes)
+#------PYGAME INIT------------
+pygame.init()
+window = pygame.display.set_mode((swidth, sheight))
+pygame.display.set_caption("STOPWATCH")
+#-----------------------------
+
+#------MAIN LOOP--------------
+while run: 
+	pygame.time.delay(60)
+
+	time.sleep(1)
+	if m!=(timel): 
+		s+=1
+		if s>59: 
+			s=0
+			m+=1
+
+	keys = pygame.key.get_pressed()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: 
+			run = False 
+ 
+	if len(str(m))!=2 and len(str(s))!= 2: 
+		t = "0" + str(m) + ":" + "0" + str(s)
+	elif len(str(m))!=2: 
+		t = "0" + str(m) + ":" + str(s)
+	elif len(str(s))!=2: 
+		t = str(m) + ":" + "0" + str(s)
+	else: 
+		t = str(m) + ":" + "0" + str(s)
+	myfont = pygame.font.SysFont('Comic Sans MS', 60)
+	timee = myfont.render(t, False, (255, 255, 255))
+	window.fill((0,0,0))
+	window.blit(timee,(75, 55))
+	pygame.display.update()
+#-------------------------------
+pygame.quit()
